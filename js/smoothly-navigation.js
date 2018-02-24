4dc4221aaa8dd0945c0f58b70a88f670c4779007
miffy24 <chenzhutao125@163.com>
2018-02-24 21:37:45 +0800

    mvc简化

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,33 +1,37 @@
 !function(){
-    let aTags = document.querySelectorAll('nav.menu > ul>li>a')
-    function animate(time) {
+    var view = document.querySelectorAll('nav.menu')
+    var controller = function(view){
+        let aTags = view.querySelectorAll('nav.menu > ul>li>a')
+        function animate(time) {
+            requestAnimationFrame(animate);
+            TWEEN.update(time);
+        }
         requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-    for(let i = 0; i<aTags.length;i++){
-        aTags[i].onclick = function(x){
-            x.preventDefault()
-            let a = x.currentTarget
-            let herf = a.getAttribute('href')
-            let element = document.querySelector(herf)
-            let top = element.offsetTop
-
-            let currentTop = window.scrollY
-            let targetTop = top -80
-            let s = targetTop- currentTop//路程
-            var coords = {y:currentTop};//起始位置
-            var t = Math.abs((s/100)*300)//时间
-            if(t>500){t = 500}
-            var tween = new TWEEN.Tween(coords) // 起始位置
-                    .to({y: targetTop }, t) // 结束位置，时间.
-                    .easing(TWEEN.Easing.Cubic.InOut) //缓动
-                    .onUpdate(function() {
-                        //coords.y变了
-                        window.scrollTo(0,coords.y)//如何更新界面
-                    })
-                    .start(); // 开始缓动
+    
+        for(let i = 0; i<aTags.length;i++){
+            aTags[i].onclick = function(x){
+                x.preventDefault()
+                let a = x.currentTarget
+                let herf = a.getAttribute('href')
+                let element = document.querySelector(herf)
+                let top = element.offsetTop
+    
+                let currentTop = window.scrollY
+                let targetTop = top -80
+                let s = targetTop- currentTop//路程
+                var coords = {y:currentTop};//起始位置
+                var t = Math.abs((s/100)*300)//时间
+                if(t>500){t = 500}
+                var tween = new TWEEN.Tween(coords) // 起始位置
+                        .to({y: targetTop }, t) // 结束位置，时间.
+                        .easing(TWEEN.Easing.Cubic.InOut) //缓动
+                        .onUpdate(function() {
+                            //coords.y变了
+                            window.scrollTo(0,coords.y)//如何更新界面
+                        })
+                        .start(); // 开始缓动
+            }
         }
     }
+    controller(view)
 }.call()
