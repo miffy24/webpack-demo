a09093423d191be00737529ef71063d78d9ee31c
miffy24 <chenzhutao125@163.com>
2018-02-26 15:03:14 +0800

    添加mvc的model

--- js/message.js
+++ js/message.js
@@ -1,49 +1,78 @@
-var APP_ID = 'amKfoP4kMJ9hFYIhvr8fM8RW-gzGzoHsz';
-var APP_KEY = 'eIRwzzyRHpUYpINENTdxIOhB';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-var query = new AV.Query('Message');
-query.find()
-  .then(
-    function (messages) {
-    let array = messages.map((item)=> item.attributes)
-    array.forEach((item) => {
-      let li = document.createElement('li')
-      li.innerText =`${item.name}:${item.content}`
-      let messageList =document.querySelector('#messageList')
-      messageList.appendChild(li)
-    })
+!function(){
+  var model = {
+    // 获取数据
+    init: function(){
+      var APP_ID = 'amKfoP4kMJ9hFYIhvr8fM8RW-gzGzoHsz';
+      var APP_KEY = 'eIRwzzyRHpUYpINENTdxIOhB';
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    fetch: function(){ 
+      var query = new AV.Query('Message');
+      return query.find() // Promise 对象
+    },
+    // 创建数据
+    save: function(name, content){
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      return message.save({  // Promise 对象
+        'name': name,
+        'content': content
+      })
+    }
   }
-)
+
+  var view = document.querySelector('section.message')
 
 
-let myForm = document.querySelector('#postMessageForm')
-postMessageForm.addEventListener('submit',function(e){
-  e.preventDefault()
-  let content =myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
-  var Message =AV.Object.extend('Message')
-  var message =new Message()
-  message.save({
-    'name':name,
-    'content': content
-  }).then(function(object) {
-    window.location.reload()
-    console.log(object)  
-  })
-})
-  // //创建TestObject表
-// var x = AV.Object.extend('chen');
-// //在表中创建一行数据
-// var o = new x();
-// //数据内容是 words: 'Hello World!'
-// //如果成功则  alert('LeanCloud Rocks!');
-// o.save({
-//   words: 'Hello World!'
-// }).then(function(object) {
-//   alert('LeanCloud Rocks!');
-// })
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function(view, model){
+      this.view = view
+      this.model = model
+
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function(){
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item)=> item.attributes )
+          array.forEach((item)=>{
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        } 
+      )
+    },
+    bindEvents: function(){
+      this.form.addEventListener('submit', function(e){
+        e.preventDefault()
+        this.saveMessage()
+      })
+    },
+    saveMessage: function(){
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name, content).then(function(object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        console.log(object)
+      })
+    }
+
+  }
+
+  controller.init(view, model)
+
+
+}.call()
